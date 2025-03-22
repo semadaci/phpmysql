70a7d1682afac861da0ceb89356b3347cd3bc0f0
semadaci <sema.daci@universitetiaab.com>
2025-03-22 08:17:33 -0700

    day8

--- day8/connection.php
+++ day8/connection.php
@@ -1,10 +1,24 @@
 <?php
 try{
-    $pdo = new PDO("mysql:host=localhost;dbnama=databae", "root", "");
+    $pdo = new PDO("mysql:host=localhost;dbname=ariana", "root", "");
 
-    $username = "Ariana";
-    $password = password_hash("mypassword", PASSWORD 
-    VALUES )
+    
+    $username = "TESTI123";
+    $password = password_hash("mypassword", PASSWORD_DEFAULT);
+  //     $email = "test@gmail.com";
+   // $sql = "INSERT INTO user(username, password) VALUES ('$username', '$password')";
+
+//     $sql= "ALTER table usrs ADD emali VARCHAR(255)"; 
+
+//     $sql = "ALTER TABLE uesrs DROP COLUMN email"; 
+
+     $sql = "DROP TABLE PRODUCTS"; 
+
+        $pdo->exec($sql);
+
+    echo "new record created succsessefully";
+}catch(DOExection $e){
+    echo $e->getMessage();
 }
 
 
